b1ed78a3937cad90384d0e6ae844f52b82a02bfa
Mong-Kuen Sun <mongkuen.sun@gmail.com>
2019-12-27 22:00:38 -0600

    Updates

---
+++ precache-manifest.911388e6d894fdd46c0dd85bf5e13484.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb639f4c87effc86efe9922d3c859c89",
+    "url": "/Notes/index.html"
+  },
+  {
+    "revision": "ad03de6884b3715c87e7",
+    "url": "/Notes/static/css/main.081c8971.chunk.css"
+  },
+  {
+    "revision": "82eab780a42c5ac2af23",
+    "url": "/Notes/static/js/2.45a22767.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "/Notes/static/js/2.45a22767.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad03de6884b3715c87e7",
+    "url": "/Notes/static/js/main.089de272.chunk.js"
+  },
+  {
+    "revision": "b38b53b27e5c4e615c46",
+    "url": "/Notes/static/js/runtime-main.45198467.js"
+  }
+]);
